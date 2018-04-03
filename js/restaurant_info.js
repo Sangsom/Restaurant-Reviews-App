8d3cd2ba4e0e394a16d54a77e9623b2259972ee9
Rinalds Pudulis <rpudulis@gmail.com>
2018-04-03 15:28:04 +0300

    feat: Setting up Service Worker

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -165,3 +165,16 @@ getParameterByName = (name, url) => {
     return '';
   return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
+
+// Registering service worker
+// https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../sw.js', { scope: '' })
+    .then(function (reg) {
+      // registration worked
+      console.log('Registration succeeded. Scope is ' + reg.scope);
+    }).catch(function (error) {
+      // registration failed
+      console.log('Registration failed with ' + error);
+    });
+}

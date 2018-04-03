fcb5e7d4ba58c954cfc71590483d5dfa1aa59a5a
Rinalds Pudulis <rpudulis@gmail.com>
2018-04-03 15:08:53 +0300

    feat: Setting up tabindexes to required eleements

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -98,6 +98,7 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
   const title = document.createElement('h2');
   title.innerHTML = 'Reviews';
+  title.setAttribute('tabindex', 0);
   container.appendChild(title);
 
   if (!reviews) {
@@ -133,6 +134,8 @@ createReviewHTML = (review) => {
   const comments = document.createElement('p');
   comments.innerHTML = review.comments;
   li.appendChild(comments);
+
+  li.setAttribute('tabindex', 0);
 
   return li;
 }
